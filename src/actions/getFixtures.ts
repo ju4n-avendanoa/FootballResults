6e6d17296404196686e3e83505af595909c4d1bf
ju4n-avendanoa <juanfernando.avendanoa@gmail.com>
2024-05-05 21:35:01 -0500

    fix: undefined error when fetching games fixture resolved

--- src/actions/getFixtures.ts
+++ src/actions/getFixtures.ts
@@ -12,7 +12,6 @@ export async function getRounds(leagueId: number, currentSeason: number) {
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result.response);
     return result.response;
   } catch (error) {
     console.error(error);
@@ -56,7 +55,6 @@ export async function getCurrentRound(leagueId: number, currentSeason: number) {
     });
 
     const result = await response.json();
-
     return result.response[0];
   } catch (error) {
     console.error(error);
--- src/app/games/[leagueName]/[leagueId]/[round]/Games.tsx
+++ src/app/games/[leagueName]/[leagueId]/[round]/Games.tsx
@@ -13,13 +13,17 @@ type Props = {
 
 async function Games({ leagueId, leagueName, round }: Props) {
   const currentSeason = await getCurrentSeason(Number(leagueId));
-  const [rounds, leagueInfo, matches] = await Promise.all([
-    getRounds(Number(leagueId), currentSeason),
+  const rounds = await getRounds(Number(leagueId), currentSeason);
+
+  round = round === "undefined" ? rounds[0] : round;
+
+  const [leagueInfo, matches] = await Promise.all([
     getInfo(Number(leagueId), currentSeason),
     getFixture(Number(leagueId), round, currentSeason),
   ]);
 
-  const currentRound = round.replaceAll("%20", " ");
+  const currentRound =
+    round !== "undefined" ? round.replaceAll("%20", " ") : rounds[0];
 
   return (
     <div className="relative flex flex-col w-full h-full lg:flex-row min-w-[320px]">
@@ -34,7 +38,7 @@ async function Games({ leagueId, leagueName, round }: Props) {
         <RoundSelector
           rounds={rounds}
           leagueId={leagueId}
-          actualRound={rounds[0]}
+          actualRound={currentRound}
           leagueName={leagueName}
         />
         <GamesFixture matches={matches} />
